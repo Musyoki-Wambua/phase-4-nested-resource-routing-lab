33378ec17b36c51d47c48f0edbc9ba09a4d2fdc4
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-03-18 22:38:45 +0300

    Completed assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,65 @@
 class ItemsController < ApplicationController
 
+
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find_by(id: params[:user_id])
+      if user
+        items = user.items 
+        render json: items, include: :user
+      else
+        render json: {error: "User not found"}, status: :not_found
+      end
+    else 
+      items = Item.all 
+      render json: items, include: :user
+    end
   end
+
+  def show
+    if params[:user_id]
+      user = User.find_by(id: params[:user_id])
+      if user 
+        item = user.items.find_by(id: params[:id])
+        if item 
+          render json: item, include: :user
+        else
+          render json: {error: "Item not found"}, status: :not_found
+        end
+      else 
+        render json: {error: "User not found"}, status: :not_found
+      end
+    else 
+      item = Item.find_by(id: params[:id])
+      if item 
+        render json: item, include: :user
+      else 
+        render json: {error: "Item not found"}, status: :not_found
+      end
+    end
+  end
+
+  def create 
+    # item = Item.create(item_params)
+    # render json: item, include: :user
+    if params[:user_id]
+      user = User.find_by(id: params[:user_id])
+      if user 
+        item = user.items.create(item_params)
+        render json: item, status: :created
+      else 
+        render json: { error: "Item not created" }, status: :unprocessable_entity
+      end
+    end
+  end
+  
+  private 
+  
+  def item_params 
+    params.permit(:name, :description, :price)
+  end
+  
+  
+
 
 end
